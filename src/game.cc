728e31fd1074fd6fb05c51c9e0c56931395dbe77
Raúl <rulokoji@gmail.com>
2024-11-13 17:51:20 +0100

    Sand implemented as an orange square

--- src/game.cc
+++ src/game.cc
@@ -35,12 +35,18 @@ SCREEN showMenuScreen(){
 
 
 SCREEN runTestLevel(){
+    std::vector<std::vector<bool>> filledMap(GetScreenWidth()/50, std::vector<bool>(GetScreenHeight()/50,true));
     Coord playerPosition;
     playerPosition.x = 0;
     playerPosition.y = 0;
     while(!IsKeyPressed(KEY_ESCAPE)){
         BeginDrawing();
             ClearBackground(RAYWHITE);
+            for(int i = 0; i < filledMap.size(); i++){
+                for(int j = 0; j < filledMap[i].size(); j++){
+                    if(filledMap[i][j]) DrawRectangle(i*50, j*50, 50, 50, ORANGE);
+                }
+            }
             DrawRectangle(playerPosition.x, playerPosition.y, 50, 50, BLACK);
         EndDrawing();
 
@@ -48,10 +54,11 @@ SCREEN runTestLevel(){
             playerPosition.y -= 50;
         if(IsKeyPressed(KEY_A) && playerPosition.x > 0)
             playerPosition.x -= 50;
-        if(IsKeyPressed(KEY_S) && GetScreenHeight() >= playerPosition.y)
+        if(IsKeyPressed(KEY_S) && GetScreenHeight() > playerPosition.y)
             playerPosition.y += 50;
-        if(IsKeyPressed(KEY_D) && GetScreenWidth() >= playerPosition.x)
+        if(IsKeyPressed(KEY_D) && GetScreenWidth() > playerPosition.x)
             playerPosition.x +=50;
+        filledMap[playerPosition.x/50][playerPosition.y/50] = false;
     }
     return MENUSCREEN;
 }
