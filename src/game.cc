463153dce28a718262fc53b404e768383c2ba513
Raúl <rulokoji@gmail.com>
2024-11-15 10:51:20 +0100

    refactoring and added two enemies

--- src/game.cc
+++ src/game.cc
@@ -10,25 +10,10 @@ using u32 = uint_least32_t;
 using engine = std::mt19937;
 
 
-
-struct Player {
-
-    int x;
-    int y;
-    Texture2D texture;
-    //std:vector<Item> objects;
-
-};
-
-struct Casilla{
-    bool filled;
-    //...
-};
-
-struct Coord{
-    int x;
-    int y;
-};
+std::random_device os_seed;
+const u32 seed = os_seed();
+engine generator(seed);
+std::uniform_int_distribution<u32> distribute(1,4);
 
 SCREEN showMenuScreen(){
     BeginDrawing();
@@ -42,68 +27,28 @@ SCREEN showMenuScreen(){
     else return MENUSCREEN;
 }
 
-
 SCREEN runTestLevel(){
     std::vector<std::vector<bool>> filledMap(GetScreenWidth()/50, std::vector<bool>(GetScreenHeight()/50,true));
-    Player p;
-    p.x = 0;
-    p.y = 0;
-    Player e;
-    e.x = 650;
-    e.y = 300;
+    Player p = initPlayer(0, 0);
+    Enemy e1 = initEnemy(650,300,RAT);
+    Enemy e2 = initEnemy(300, 450, BAT);
     Player w;
     w.x = GetScreenWidth()-50;
     w.y = GetScreenHeight()-50;
-    std::random_device os_seed;
-    const u32 seed = os_seed();
-    engine generator(seed);
-    std::uniform_int_distribution<u32> distribute(1,4);
+    //Uniform random umber generator from 1 to 4
     float moveTimer = 0.0f;
-    std::queue<char> playerMoves;
+
+
     while(!IsKeyPressed(KEY_ESCAPE)){
 
-        moveTimer += GetFrameTime();
-        if(IsKeyDown(KEY_W))// && p.y > 0)
-            playerMoves.push('w');//p.y -= 50;
-        if(IsKeyDown(KEY_A))// && p.x > 0)
-            playerMoves.push('a');//p.x -= 50;
-        if(IsKeyDown(KEY_S))// && GetScreenHeight()-50 > p.y)
-            playerMoves.push('s');//p.y += 50;
-        if(IsKeyDown(KEY_D))// && GetScreenWidth()-50 > p.x)
-            playerMoves.push('d');//p.x +=50;
+        p.moveTime += GetFrameTime();
+        e1.moveTime += GetFrameTime();
+        e2.moveTime += GetFrameTime();
 
-        if(moveTimer >=MOVE_INTERVAL){
-            //Player movement
-            if(!playerMoves.empty()){
-                char Pmov = playerMoves.back();
-                if(Pmov == 'w' && p.y > 0)
-                    p.y -= 50;
-                if(Pmov == 'a' && p.x > 0)
-                    p.x -= 50;
-                if(Pmov == 's' && GetScreenHeight()-50 > p.y)
-                    p.y += 50;
-                if(Pmov == 'd' && GetScreenWidth()-50 > p.x)
-                    p.x +=50;
-                filledMap[p.x/50][p.y/50] = false;
-            }
-            //Enemy movement
-            int Emov = distribute(generator);
-            if(Emov==1 && e.y > 0)
-                e.y -= 50;
-            if(Emov==2 && e.x > 0)
-                e.x -= 50;
-            if(Emov==3 && GetScreenHeight()-50 > e.y)
-                e.y += 50;
-            if(Emov==4 && GetScreenWidth()-50 > e.x)
-                e.x +=50;
-
-            filledMap[e.x/50][e.y/50] = false;
-            moveTimer = 0.0f;
-
-            //Clear movement queue
-            playerMoves = std::queue<char>();
-
-        }
+        checkPlayerMovement(p);
+        movePlayer(p, filledMap);
+        moveEnemy(e1, filledMap);
+        moveEnemy(e2, filledMap);
 
         BeginDrawing();
             ClearBackground(RAYWHITE);
@@ -112,15 +57,90 @@ SCREEN runTestLevel(){
                     if(filledMap[i][j]) DrawRectangle(i*50, j*50, 50, 50, ORANGE);
                     }
                 }
-
             DrawRectangle(w.x, w.y, 50, 50, GREEN);
             DrawRectangle(p.x, p.y, 50, 50, BLACK);
-            DrawRectangle(e.x,e.y, 50, 50, RED);
+            DrawRectangle(e1.x,e1.y, 50, 50, RED);
+            DrawRectangle(e2.x,e2.y, 50, 50, RED);
+
         EndDrawing();
         //Check player's death'
-        if(e.x == p.x && e.y == p.y) return MENUSCREEN;
+        if(e1.x == p.x && e1.y == p.y || e2.x == p.x && e2.y == p.y) return MENUSCREEN;
         if(w.x == p.x && w.y == p.y) return LVL1;
     }
     return MENUSCREEN;
+}
 
+Player initPlayer(int x, int y){
+    Player p;
+    p.x = x;
+    p.y = y;
+    p.moveTime = 0;
+    std::queue<char> playerMoves;
+    //Further initialization steps...
+    return p;
+}
+
+Enemy initEnemy(int x, int y, ETYPE type){
+    Enemy e;
+    e.x = x;
+    e.y = y;
+    e.type = type;
+    e.moveTime = 0;
+    //switch(type) //set texture according to the enemy type
+    return e;
+}
+
+void checkPlayerMovement(Player &p){
+
+    if(IsKeyDown(KEY_W))// && p.y > 0)
+        p.playerMoves.push('w');//p.y -= 50;
+    if(IsKeyDown(KEY_A))// && p.x > 0)
+        p.playerMoves.push('a');//p.x -= 50;
+    if(IsKeyDown(KEY_S))// && GetScreenHeight()-50 > p.y)
+        p.playerMoves.push('s');//p.y += 50;
+    if(IsKeyDown(KEY_D))// && GetScreenWidth()-50 > p.x)
+        p.playerMoves.push('d');//p.x +=50;
+}
+
+void movePlayer(Player &p, std::vector<std::vector<bool>> &map){
+
+    if(p.moveTime>=MOVE_INTERVAL){
+        //Player movement
+        if(!p.playerMoves.empty()){
+            char Pmov = p.playerMoves.back();
+            if(Pmov == 'w' && p.y > 0)
+                p.y -= 50;
+            if(Pmov == 'a' && p.x > 0)
+                p.x -= 50;
+            if(Pmov == 's' && GetScreenHeight()-50 > p.y)
+                p.y += 50;
+            if(Pmov == 'd' && GetScreenWidth()-50 > p.x)
+                p.x +=50;
+            map[p.x/50][p.y/50] = false;
+        }
+        p.moveTime = 0.0f;
+        //Clear movement queue
+        p.playerMoves = std::queue<char>();
+    }
+}
+
+
+void moveEnemy(Enemy &e, std::vector<std::vector<bool>> &map){
+
+    if(e.moveTime >= BAT_MOVE_INTERVAL){
+        //Enemy movement
+        int Emov = distribute(generator);
+        if(Emov==1 && e.y > 0)
+            e.y -= 50;
+        if(Emov==2 && e.x > 0)
+            e.x -= 50;
+        if(Emov==3 && GetScreenHeight()-50 > e.y)
+            e.y += 50;
+        if(Emov==4 && GetScreenWidth()-50 > e.x)
+            e.x +=50;
+
+        map[e.x/50][e.y/50] = false;
+
+        e.moveTime = 0.0f;
+    }
 }
--- src/game.hh
+++ src/game.hh
@@ -1,5 +1,12 @@
+#include "raylib.h"
+#include <queue>
 
 const float MOVE_INTERVAL = 0.2f;
+const float BAT_MOVE_INTERVAL = 3*MOVE_INTERVAL;
+const float SCORPION_MOVE_INTERVAL = 2*MOVE_INTERVAL;
+const float RAT_MOVE_INTERVAL = MOVE_INTERVAL;
+const float SNAKE_MOVE_INTERVAL = 5*MOVE_INTERVAL;
+
 
 typedef enum {
     LVL1,
@@ -9,7 +16,51 @@ typedef enum {
 
 } SCREEN;
 
+typedef enum {
+    SCORPION,
+    RAT,
+    BAT,
+    SNAKE,
+} ETYPE;
+
+struct Player {
+    int x;
+    int y;
+    float moveTime;
+    std::queue<char> playerMoves;
+    Texture2D texture;
+    //std:vector<Item> objects;
+};
+
+struct Enemy {
+    int x;
+    int y;
+    float moveTime;
+    Texture2D texture;
+    ETYPE type;
+};
+
+struct Casilla{
+    bool filled;
+    //...
+};
+
+struct Coord{
+    int x;
+    int y;
+};
 
 SCREEN showMenuScreen();
 
 SCREEN runTestLevel();
+
+Player initPlayer(int x, int y);
+
+Enemy initEnemy(int x, int y, ETYPE type);
+
+
+void checkPlayerMovement(Player &p);
+
+void movePlayer(Player &p, std::vector<std::vector<bool>> &map);
+
+void moveEnemy(Enemy &e, std::vector<std::vector<bool>> &map);
